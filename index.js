6dd20475a71a098496dd7087c6e727ac565e4744
tanya-pvt2005 <tanya.pvt2005@gmail.com>
2025-11-24 15:02:54 +0530

    added model folder

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ console.log("server code")
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+mongoose.connect("mongodb+srv://tanyapvt2005_db_user:BzV7o5SJ2V3jmBxC@forevercluster0.c1dsmus.mongodb.net")
+.then(()=>console.log("MongoDB connected"))
+
 app.get("/",(req, res)=>{
     res.json({message:"ok"})
 })
